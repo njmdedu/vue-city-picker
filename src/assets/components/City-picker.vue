621554e7f693d58a04e21cdc9ea850f67c55c089
zhangwei900808 <zw900808@gmail.com>
2017-02-15 10:42:24 +0800

    init vue project

---
+++ src/assets/components/City-picker.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      message:'测试'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
